314fa95a7ae1ff6c267d1b9260925ef6f020ad95
SreeSingamsetty <39523400+SreeSingamsetty@users.noreply.github.com>
2018-12-11 14:57:00 +0100

    Delete B.adjusting_tab_embeddings.py

--- thesis/B.adjusting_tab_embeddings.py
+++
@@ -1,7 +0,0 @@
-import regex as re
-with open("16_allemb.emb","r") as f:
-    with open("16_allemb.emb","w") as w:
-        p = re.compile('([a-z]|[.]|\))\s(-[0-9][.])|([a-z]|[.]|\))\s([0-9][.])')
-        for line in f:
-            line=re.sub(r'([a-z]|[.]|\))\s(-*[0-9][.])',r'\1\t\2',line)#([a-z]d{4}\-\d{2})
-            w.write(line)
